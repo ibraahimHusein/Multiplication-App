196ddd1b46dd599ec75c2bddb7e01182d5ca9a21
ibraahimHusein <ibrahusein199@gmail.com>
2025-12-19 19:56:02 +0300

    Add files via upload

---
+++ multiplication App.js
@@ -0,0 +1,31 @@
+const num1 = Math.ceil(Math.random() * 10);
+const num2 = Math.ceil(Math.random() * 10);
+
+const questionEl = document.getElementById("question");
+const inputEl = document.getElementById("input");
+const formEl = document.getElementById("form");
+const scoreEl = document.getElementById("Score");
+
+let score = JSON.parse(localStorage.getItem("score"));
+if (!score) {
+    score = 0;
+}
+
+
+score.innerText = `score: ${score}`;
+questionEl.innerText = `what is ${num1} multiply by ${num2}?`;
+const correctAns = num1 * num2;
+
+formEl.addEventListener("submit", () => {
+    const userAns = +inputEl.value;
+    if (userAns === correctAns){
+        score++
+        updatelocalstorage();
+    }else {
+        score--;
+        updatelocalstorage();
+    }
+});
+function updatelocalstorage() {
+    localStorage.setItem("score",json.stringify(score));
+}
